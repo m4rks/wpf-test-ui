a5fd29899cd693237fba5cd2d8699bd8cfc590b6
Marek Michalak <michalak.marek@gmail.com>
2023-01-18 18:48:10 +0100

    add pump task view model

---
+++ wpf-test-ui/Common/ViewModelLocator.cs
@@ -0,0 +1,8 @@
+﻿namespace wpf_test_ui.ViewModels
+{
+    public class ViewModelLocator
+    {
+        //public PumpControlViewModel PumpControlViewModel => new PumpControlViewModel(App.Navigation, new MockCustomersService());
+        public TasksViewModel TasksViewModel => new TasksViewModel();
+    }
+}
--- wpf-test-ui/ViewModels/PumpControlViewModel.cs
+++ wpf-test-ui/ViewModels/PumpControlViewModel.cs
@@ -8,5 +8,37 @@ namespace wpf_test_ui.ViewModels
 {
     public class PumpControlViewModel : BaseViewModel
     {
+        #region Field
+        public RelayCommand ShowTaskForPumpCommand { get; set; }
+        #endregion
+
+        #region Property
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        public PumpControlViewModel()
+        {
+            Name = "pump #1";
+            ShowTaskForPumpCommand = new RelayCommand(p => ShowTaskForPump());
+        }
+
+        #region Property
+        public void ShowTaskForPump()
+        {
+
+        }
+        #endregion
+
+
+
     }
 }
